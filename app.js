ba1c776a705e389d7e23925e739d3d1c8ce7d8db
Ariane Nascimento <teacher.ariane.lima@gmail.com>
2025-08-02 01:25:02 +0100

    alterando limite para 100

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ titulo.innerHTML = 'Jogo do Número Secreto';*/
 
 /*let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número de 1 e 10';*/
-
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
+let tentaivas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilain Portuguese Female', {rate:1.2})
 }
-
+function exibirMensagemInicial() {
 exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 exibirTextoNaTela('p', 'Escolha um número de 1 e 100');
+}
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = documet
